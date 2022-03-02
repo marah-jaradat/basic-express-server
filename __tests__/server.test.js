9193449cb39606b105287307b6cdd60678edb583
marah-jaradat <marahjaradat97@gmail.com>
2022-03-02 18:50:00 +0300

    adding 404 &500 test

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,19 +1,38 @@
 "use strict";
 const server = require("../src/server.js");
-
 const supertest = require("supertest");
-
 const request = supertest(server.app);
 
-describe("testing my server", () => {
-  it("testing/", async () => {
-    const response = await request.get("/");
-    // console.log(response);
-    expect(response.text).toEqual("home route");
+describe("testing my API server", () => {
+  beforeEach(() => {
+    jest.spyOn(console, "log").mockImplementation();
   });
 
-  it("testing/person", async () => {
+  it("should  get status 500", async () => {
+    const response = await request.get("/person?name=");
+    expect(response.status).toBe(500);
+  });
+
+  it("handles not found request", async () => {
+    const response = await request.get("/wrongPath");
+    expect(response.status).toEqual(404);
+  });
+
+  it("bad method", async () => {
+    const response = await request.post("/");
+    expect(response.status).toEqual(404);
+  });
+
+  it("testing /", async () => {
+    const response = await request.get("/");
+    expect(response.status).toEqual(200);
+    expect(response.text).toEqual("home route");
+    // console.log(response.text);
+  });
+
+  it("testing /person", async () => {
     const response = await request.get("/person");
+    // expect(response.status).toEqual(200);
     expect(typeof response.body).toEqual("object");
   });
 });
